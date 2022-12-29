15a82df9b5b7a1eb7f151fabf7c44b1e642c7690
baochi0212 <chideptraika29@gmail.com>
2022-12-29 14:19:11 +0700

    initg

--- source/data_utils.py
+++ source/data_utils.py
@@ -180,7 +180,7 @@ def load_data(dataset, data_dir, tokenizer, train_batch_size, test_batch_size, m
     testset = MyDataset(test_data, label_dict, tokenizer, model_name, method)
     collate_fn = partial(my_collate, tokenizer=tokenizer, method=method, num_classes=len(label_dict))
     train_dataloader = DataLoader(trainset, train_batch_size, shuffle=True, num_workers=workers, collate_fn=collate_fn, pin_memory=True)
-    test_dataloader = DataLoader(testset, test_batch_size, shuffle=False, num_workers=workers, collate_fn=collate_fn, pin_memory=True)
+    test_dataloader = DataLoader(testset, test_batch_size, shuffle=True, num_workers=workers, collate_fn=collate_fn, pin_memory=True)
     return train_dataloader, test_dataloader
 
 if __name__ == '__main__':
