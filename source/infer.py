631029f5804f609ab515dc3ac47a8bff929b2736
baochi0212 <chideptraiak29@gmail.com>
2023-02-03 21:39:47 +0700

    .

---
+++ source/infer.py
@@ -0,0 +1,21 @@
+import torch
+from tqdm import tqdm
+from model import Transformer
+from config import get_config
+import torch.nn as nn
+from loss_func import CELoss, SupConLoss, DualLoss
+from data_utils import load_data, text2dict
+from transformers import logging, AutoTokenizer, AutoModel, BertModel, BertConfig, get_linear_schedule_with_warmup
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import os
+
+save_path = os.environ['SAVE_MODEL']
+args, logger = get_config()
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+base_model = AutoModel.from_pretrained('bert-base-uncased')
+model = Transformer(base_model, args.num_classes, args.method)
+model = model.load_state_dict(torch.load(save_path))
+
